9f75d192d962bd15ea65af5d0bcae90e0a11ae2e
Ryan Evans <rdevans87@gmail.com>
2021-06-20 02:48:59 -0400

    server.js from previous hw to save time

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const logger = require("morgan")
+ const logger = require("morgan")
+
+onst PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Concise output colored by response status for development use.
+app.use(logger("dev"));
+
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
