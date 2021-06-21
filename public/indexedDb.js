968cc5c19b27ddd14c30a206b62238f462fccbd2
Ryan Evans <rdevans87@gmail.com>
2021-06-21 12:19:17 -0400

    completed functions for save and added data to db

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -16,13 +16,13 @@
 let db;
 
 const request = indexedDB.open('budget', 1);
-request.onupgradeneeded = function (evt) {
+request.onupgradeneeded = function(evt) {
     const db = evt.target.result;
-    db.createObject('new_transaction', { autoIncrement: true });
+    db.createObjectStore('new_transaction', { autoIncrement: true });
 
 };
 
-request.onsuccess = function (evt) {
+request.onsuccess = function(evt) {
     db = evt.target.result;
     if (navigator.onLine) {
         uploadTransaction();
@@ -31,43 +31,56 @@ request.onsuccess = function (evt) {
 };
 
 request.onError = function(evt) {
-console.log(evt.target.errCode)
+    console.log(evt.target.errCode)
 
 };
 
 function saveRecord(rec) {
-const transaction = db.transaction(['new_transaction'], 'readWrite'); 
-const budgetObjectStore = transaction.objectStore('new_transaction');
-budgetObjectStore.add(rec);
+    const transaction = db.transaction(['new_transaction'], 'readWrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+    budgetObjectStore.add(rec);
 
 };
 
-if (getAll.result.length > 0) {
-fetch('/api/transaction', {
-    method: 'POST',
-    body: JSON.stringify(getAll.result),
-    headers: {
-        Accept: 'application/json, text/plain, */*',
-       'Content-Type': 'application/json'
-    }
-})
-.then(response => response.json())
-.then(serverResponse => {
-    if (serverResponse.message) {
-    throw newError(serverResponse);
 
-}
+function uploadTransaction(rec) {
+    const transaction = db.transaction(['new_transaction'], 'readWrite');
+    const budgetObjectStore = transaction.objectStore('new_transaction');
 
-});
+    const getAll = budgetObjectStore.getAll();
 
-const transaction = db.transaction(['new_transaction'], 'readWrite');
-const budgetObjectStore = transaction.objectStore('new_transaction');
-budgetObjectStore.clear();
-alters('Saved Transactions' )
+    getAll.onSuccess = function() {
+
+    if (getAll.results.length > 0) {
+        fetch('/api/transaction', {
+            method: 'POST',
+            body: JSON.stringify(getAll.results),
+            headers: {
+                Accept: 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if (serverResponse.message) {
+                    throw newError(serverResponse);
+
+                }
 
 
+        const transaction = db.transaction(['new_transaction'], 'readWrite');
+        const budgetObjectStore = transaction.objectStore('new_transaction');
+        
+        budgetObjectStore.clear();
 
-
-
+        alert('Saved Transactions');
+     })
+       .catch(err => {
+           console.log(err);
+     });
+    
+    } 
+  }  
+};
 
 window.addEventListener('online', checkDatabase)
