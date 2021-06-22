286182ea23faa33d331bce117d4da9f9a2860685
Ryan Evans <rdevans87@gmail.com>
2021-06-22 12:47:26 -0400

    full refactor of service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,32 +47,16 @@ self.addEventListener("fetch", function(event) {
   }
 
 
-
-self.addEventListener('fetch', function(event) {
-  if (event.request.url.includes("/api/")) {
-    event.respondWith(
-      caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(event.request)
-          .then(response => {
-            if (response.status === 200) {
-              cache.put(event.request.url, response.clone());
-            }
-            return response;
-          })
-          .catch(error => {
-
-            return cache.match(event.request);
-          })
-      }).catch(error => console.log(error))
-    );
-    return;
-  }
   event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request);
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
       });
     })
   );
-
 });
