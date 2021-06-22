8b2aa3f6c495cf0486ff36140f97f8d0c4af3905
Ryan Evans <rdevans87@gmail.com>
2021-06-22 12:45:40 -0400

    full refctor of service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,36 +12,39 @@ const FILES_TO_CACHE = [
 ];
 
 
-
-// install
-self.addEventListener("install", function (event) {
+self.addEventListener("install", function(event) {
+  // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addALL("/icons/icons/"))
-
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
-  event.waitUntil(
-    caches.open(DATA_CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-      .then(self.skipWaiting())
-  );
-
 });
 
-  self.addEventListener("activate", function (event) {
-    event.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
             }
+
+            return response;
           })
-        );
-      })
+          .catch(err => {
+            
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
 
-    self.clients.claim();
-  });
+    return;
+  }
 
 
 
