2b516b800ae91628a53b3444d031e2530af1ffac
Ryan Evans <rdevans87@gmail.com>
2021-06-21 21:56:48 -0400

    final push before submission able to download from browser to pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,64 +16,65 @@ const FILES_TO_CACHE = [
 
 self.addEventListener('install', function (event) {
 
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(FILES_TO_CACHE)
-      .then(self.skipWaiting())
-    })
-  );
-
-});
-
-self.addEventListener('fetch', function (event) {
-    if (event.request.url.includes('/api/')) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(event.request)
-            .then((response => {
-              if (response.status === 200) {
-                cache.put(event.request.url, response.clone());
-              }
-              return response;
-            })
-              .catch(error => {
-
-                return cache.match(event.request);
-              })
-            ).catch(error => console.log(error))
-        })
-      );
-    };
-    return;
-  });
-
-
-  self.addEventListener('fetch', function (event) {
-    if (event.request.url.startsWith(self.location.origin)) {
-      event.responseWith(
-        caches.match(event.request).then((response) => {
-          if (response) {
-            return response;
-          }
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          })
-        })
-      );
-    }
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(FILES_TO_CACHE)
+        .then(self.skipWaiting())
+      })
+    );
   
-
-  event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.match(event.request).then(response => {
-        return response || fetch(event.request);
-      });
-
-    })
-
- );
-
-});
+  });
+  
+  self.addEventListener('fetch', function (event) {
+      if (event.request.url.includes('/api/')) {
+        event.respondWith(
+          caches.open(DATA_CACHE_NAME).then(cache => {
+            return fetch(event.request)
+              .then((response => {
+                if (response.status === 200) {
+                  cache.put(event.request.url, response.clone());
+                }
+                return response;
+              })
+                .catch(error => {
+  
+                  return cache.match(event.request);
+                })
+              ).catch(error => console.log(error))
+          })
+        );
+      };
+      return;
+    });
+  
+  
+    self.addEventListener('fetch', function (event) {
+      if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+          caches.match(event.request).then((response) => {
+            if (response) {
+              return response;
+            }
+            return fetch(event.request).then((response) => {
+              return cache.put(event.request, response.clone()).then(() => {
+                return response;
+              });
+            })
+          })
+        );
+      }
+    
+  
+    event.respondWith(
+      caches.open(CACHE_NAME).then(cache => {
+        return cache.match(event.request).then(response => {
+          return response || fetch(event.request);
+        });
+  
+      })
+  
+   );
+  
+  });
+  
